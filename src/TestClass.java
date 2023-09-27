93bd3b163936793b1d4aba335342d16fcf4261ee
wechnerjulian <145550176+wechnerjulian@users.noreply.github.com>
2023-09-27 09:34:00 +0200

    Jw/test2 (#2)
    * gets
    
    * julian
    
    * neue Variablen
    
    ---------
    
    Co-authored-by: julian_wechner <juliawechner@tsn.at>

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,11 @@ public class TestClass {
     private int lol = 7;
     private String test3;
     private int test2 = 8;
+    private String test9;
+    private String test4;
+    private int test5 = 8;private String test6;
+    private String test7;
+    private int test0 = 8;
+
 
 }
