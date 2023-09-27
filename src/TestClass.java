9b062d5efe28f934a28e5c0a351eb72f66dd59a5
julian_wechner <juliawechner@tsn.at>
2023-09-27 09:30:34 +0200

    neue Variablen

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,4 +2,10 @@ public class TestClass {
     private String test;
     private String test3;
     private int test2 = 8;
+    private String test9;
+    private String test4;
+    private int test5 = 8;private String test6;
+    private String test7;
+    private int test0 = 8;
+
 }
