f369d7d9f8b460d885a489466fa51164b49d2e52
sinanue06 <126083029+sinanue06@users.noreply.github.com>
2023-09-27 09:36:29 +0200

    sinanTestNeu (#3)

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,13 +1,20 @@
+
+
 public class TestClass {
     private String test;
     private int lol = 7;
     private String test3;
     private int test2 = 8;
+
+    private int uebung = 27923;
+    private double uebung1 = 27.9;
+
     private String test9;
     private String test4;
     private int test5 = 8;private String test6;
     private String test7;
     private int test0 = 8;
+
 
 
 }
